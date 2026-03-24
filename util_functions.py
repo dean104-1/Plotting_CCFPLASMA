e957245e1f892d5a630100e0d1457f9c62ff2e56
Matthew Dean <dean104@raider03.afrl.hpc.mil>
2026-03-24 17:56:02 -0400

    Updated util_functions to create .pkl files for easier re-plotting

--- util_functions.py
+++ util_functions.py
@@ -1,40 +1,49 @@
 import numpy as np
 import struct
+import pickle
+from pathlib import Path
 
 def loadslices(fname,nslices):
-    slices = []
-    with open(fname, "rb") as f:
-        for ii in range(nslices):
-        #with open(fname, "rb") as f:
-           
-           f.read(4)  # skip record marker
-           IGRID = np.fromfile(f, dtype=np.int32, count=1)
-           ISTEP = np.fromfile(f, dtype=np.int32, count=1)
-           NJ = np.fromfile(f, dtype=np.int32, count=1)
-           NK = np.fromfile(f, dtype=np.int32, count=1)
-           NL = np.fromfile(f, dtype=np.int32, count=1)
-           NQ = np.fromfile(f, dtype=np.int32, count=1)
-           NQC = np.fromfile(f, dtype=np.int32, count=1)
-           IGRID = int(np.asarray(IGRID).item());ISTEP = int(np.asarray(ISTEP).item())
-           NJ = int(np.asarray(NJ).item()); NK = int(np.asarray(NK).item()); NL = int(np.asarray(NL).item())
-           NQ = int(np.asarray(NQ).item()); NQC = int(np.asarray(NQC).item())
-           TVREF = np.fromfile(f, dtype=np.float64, count=1)
-           DTVREF = np.fromfile(f, dtype=np.float64, count=1)
-           X = np.fromfile(f, dtype=np.float64, count=NJ*NK*NL)
-           Y = np.fromfile(f, dtype=np.float64, count=NJ*NK*NL)
-           Z = np.fromfile(f, dtype=np.float64, count=NJ*NK*NL)
-           Q = np.fromfile(f, dtype=np.float64, count=NJ*NK*NL*NQ)
-           IBLANK = np.fromfile(f, dtype=np.int32, count=NJ*NK*NL)
-           X = X.reshape((NJ,NK,NL), order='F')
-           Y = Y.reshape((NJ,NK,NL), order='F')
-           Z = Z.reshape((NJ,NK,NL), order='F')
-           Q = Q.reshape((NJ,NK,NL,NQ), order='F')
-           IBLANK = IBLANK.reshape((NJ,NK,NL), order='F')
-           f.read(4)  # skip end marker
-           
-           slices.append({"IGRID":IGRID, "ISTEP":ISTEP, "NJ":NJ, "NK":NK, "NL":NL,
-                          "NQ":NQ, "NQC":NQC, "TVREF":TVREF, "DTVREF":DTVREF,
-                          "X":X, "Y":Y, "Z":Z, "Q":Q})
+    if(Path(f"{fname}.pkl").exists()):
+        with open(f"{fname}.pkl", "rb") as g:
+            slices = pickle.load(g)
+
+    else:
+        slices = [None]*nslices
+        with open(fname, "rb") as f:
+            for ii in range(nslices):
+            
+               f.read(4)  # skip record marker
+               IGRID = np.fromfile(f, dtype=np.int32, count=1)
+               ISTEP = np.fromfile(f, dtype=np.int32, count=1)
+               NJ = np.fromfile(f, dtype=np.int32, count=1)
+               NK = np.fromfile(f, dtype=np.int32, count=1)
+               NL = np.fromfile(f, dtype=np.int32, count=1)
+               NQ = np.fromfile(f, dtype=np.int32, count=1)
+               NQC = np.fromfile(f, dtype=np.int32, count=1)
+               IGRID = int(np.asarray(IGRID).item());ISTEP = int(np.asarray(ISTEP).item())
+               NJ = int(np.asarray(NJ).item()); NK = int(np.asarray(NK).item()); NL = int(np.asarray(NL).item())
+               NQ = int(np.asarray(NQ).item()); NQC = int(np.asarray(NQC).item())
+               TVREF = np.fromfile(f, dtype=np.float64, count=1)
+               DTVREF = np.fromfile(f, dtype=np.float64, count=1)
+               X = np.fromfile(f, dtype=np.float64, count=NJ*NK*NL)
+               Y = np.fromfile(f, dtype=np.float64, count=NJ*NK*NL)
+               Z = np.fromfile(f, dtype=np.float64, count=NJ*NK*NL)
+               Q = np.fromfile(f, dtype=np.float64, count=NJ*NK*NL*NQ)
+               IBLANK = np.fromfile(f, dtype=np.int32, count=NJ*NK*NL)
+               X = X.reshape((NJ,NK,NL), order='F')
+               Y = Y.reshape((NJ,NK,NL), order='F')
+               Z = Z.reshape((NJ,NK,NL), order='F')
+               Q = Q.reshape((NJ,NK,NL,NQ), order='F')
+               IBLANK = IBLANK.reshape((NJ,NK,NL), order='F')
+               f.read(4)  # skip end marker
+
+               slices[ii] = {"IGRID":IGRID, "ISTEP":ISTEP, "NJ":NJ, "NK":NK, "NL":NL,
+                              "NQ":NQ, "NQC":NQC, "TVREF":TVREF, "DTVREF":DTVREF,
+                              "X":X, "Y":Y, "Z":Z, "Q":Q}
+
+        with open(f"{fname}.pkl", "wb") as g:
+            pickle.dump(slices, g, protocol=pickle.HIGHEST_PROTOCOL)
 
     return slices
 
